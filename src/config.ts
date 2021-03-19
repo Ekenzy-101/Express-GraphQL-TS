a9043f4cc01ea6cdd1fb476f005f25238a40d2d8
Ekenzy-101 <ekeneonyekaba@gmail.com>
2021-03-19 15:08:57 +0100

    Added the login mutation

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+export const APP_ACCESS_SECRET = process.env.APP_ACCESS_SECRET!;
+
+export const ACCESS_TOKEN_COOKIE_NAME = "token";
+
 export const DB_CONNECTION = (process.env.DB_CONNECTION || "postgres") as
   | "postgres"
   | "mysql";
 export const DATABASE_URL = process.env.DATABASE_URL!;
 
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
-
-export const APP_ACCESS_SECRET = process.env.APP_ACCESS_SECRET!;
--- src/resolvers/auth.ts
+++ src/resolvers/auth.ts
@@ -1,8 +1,9 @@
 import { ApolloError, ExpressContext } from "apollo-server-express";
-import { getValidationErrors } from "../utils/validation";
 import { Args, ArgsType, Ctx, Field, Mutation, Resolver } from "type-graphql";
+import bcrypt from "bcrypt";
 
-import { IS_PRODUCTION } from "../config";
+import { ACCESS_TOKEN_COOKIE_NAME, IS_PRODUCTION } from "../config";
+import { getValidationErrors } from "../utils/validation";
 import { User } from "../entities/User";
 
 @ArgsType()
@@ -33,16 +34,47 @@ export class AuthResolver {
     if (errors) throw new ApolloError(JSON.stringify(errors));
 
     let user = await User.findOne({ where: { email: args.email } });
-
     if (user) {
       throw new ApolloError(JSON.stringify({ email: "Email already exists" }));
     }
 
-    user = await User.create(args).save();
+    const hashedPassword = await bcrypt.hash(args.password, 12);
+    user = await User.create({ ...args, password: hashedPassword }).save();
     const token = user.generateAccessToken();
     const oneDay = 1000 * 60 * 60 * 24;
 
-    res.cookie("token", token, {
+    res.cookie(ACCESS_TOKEN_COOKIE_NAME, token, {
+      httpOnly: true,
+      secure: IS_PRODUCTION,
+      maxAge: oneDay,
+    });
+
+    return user;
+  }
+
+  @Mutation(() => User)
+  async login(@Args() args: LoginArgs, @Ctx() { res }: ExpressContext) {
+    const schema = {
+      email: "email|max:255",
+      password: "string|min:6",
+    };
+    const errors = getValidationErrors(args, schema);
+    if (errors) throw new ApolloError(JSON.stringify(errors));
+
+    let user = await User.findOne({ where: { email: args.email } });
+    if (!user) {
+      throw new ApolloError("Invalid Email or Password");
+    }
+
+    const isValid = await bcrypt.compare(args.password, user.password);
+    if (!isValid) {
+      throw new ApolloError("Invalid Email or Password");
+    }
+
+    const token = user.generateAccessToken();
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    res.cookie(ACCESS_TOKEN_COOKIE_NAME, token, {
       httpOnly: true,
       secure: IS_PRODUCTION,
       maxAge: oneDay,
